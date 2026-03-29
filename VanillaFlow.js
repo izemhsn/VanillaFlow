359ad28ef57a06e99d366c796cd8270f2455e7c1
El Houssaine Izem <izem.hsn@gmail.com>
2026-03-29 22:31:26 +0100

    Refactor VanillaFlow: simplify evaluate with dot notation, add x-text/x-html directives, improve init with constructor

--- VanillaFlow.js
+++ VanillaFlow.js
@@ -1,8 +1,14 @@
 export default class VanillaFlow {
+    constructor(data = {}) {
+        this.data = data;
+    }
+
     init() {
         this.directives = {
             'x-if': this.processIf.bind(this),
             'x-show': this.processShow.bind(this),
+            'x-text': this.processText.bind(this),
+            'x-html': this.processHtml.bind(this),
         };
         
         for (const [attr, handler] of Object.entries(this.directives)) {
@@ -13,22 +19,30 @@ export default class VanillaFlow {
         return this;
     }
 
-    processIf(el, condition) {
-        if (!this.evaluate(condition)) {
+    processIf(el, expression) {
+        if (!this.evaluate(expression)) {
             el.remove();
         }
     }
 
-    processShow(el, condition) {
-        el.style.display = this.evaluate(condition) ? '' : 'none';
+    processShow(el, expression) {
+        el.style.display = this.evaluate(expression) ? '' : 'none';
     }
 
-    evaluate(expression) {
-        try {
-            return new Function(`return ${expression};`)();
-        } catch {
-            return undefined;
+    processText(el, expression) {
+        el.textContent = this.evaluate(expression);
+    }
+
+    processHtml(el, expression) {
+        el.innerHTML = this.evaluate(expression);
+    }
+
+    evaluate(path) {
+        let value = this.data;
+        for (const key of path.split('.')) {
+            value = value?.[key];
         }
+        return value;
     }
 }
 
