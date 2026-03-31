69e99472ed2a8742c8810946b847eb8e83493de2
El Houssaine Izem <izem.hsn@gmail.com>
2026-03-31 21:36:24 +0100

    Add x-bind directive with expression support and boolean attribute handling

--- VanillaFlow.js
+++ VanillaFlow.js
@@ -9,6 +9,7 @@ export default class VanillaFlow {
             'x-show': this.processShow.bind(this),
             'x-text': this.processText.bind(this),
             'x-html': this.processHtml.bind(this),
+            'x-bind': this.processBind.bind(this),
         };
         
         for (const [attr, handler] of Object.entries(this.directives)) {
@@ -16,6 +17,21 @@ export default class VanillaFlow {
                 handler(el, el.getAttribute(attr));
             });
         }
+        
+        document.querySelectorAll('*').forEach(el => {
+            const attrs = Array.from(el.attributes).filter(a => a.name.startsWith('x-bind:'));
+            for (const attr of attrs) {
+                const attrName = attr.name.slice(7);
+                const value = this.evaluate(attr.value);
+                el.removeAttribute(attr.name);
+                if (value === true) {
+                    el.setAttribute(attrName, '');
+                } else if (value === false || value === null || value === undefined) {
+                } else {
+                    el.setAttribute(attrName, value);
+                }
+            }
+        });
         return this;
     }
 
@@ -37,12 +53,21 @@ export default class VanillaFlow {
         el.innerHTML = this.evaluate(expression);
     }
 
-    evaluate(path) {
-        let value = this.data;
-        for (const key of path.split('.')) {
-            value = value?.[key];
+    processBind(el, expression) {
+        el.value = this.evaluate(expression);
+    }
+
+    evaluate(expression) {
+        try {
+            const func = new Function(...Object.keys(this.data), `return ${expression}`);
+            return func(...Object.values(this.data));
+        } catch {
+            let value = this.data;
+            for (const key of expression.split('.')) {
+                value = value?.[key];
+            }
+            return value;
         }
-        return value;
     }
 }
 
